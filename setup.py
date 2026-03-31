e7883a8aa48b005619ef1aa83c3e4ed4269427a6
ISviterI <clashtop.company@gmail.com>
2026-03-31 22:06:18 +0300

    Added url to setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=">=3.6",
+    url="https://github.com/ISviterI/dashcode",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
